e922f9b83d08c005de43a3c87a6e3ec38fc355be
Sebastian Nagel <sebastian.nagel@uni-konstanz.de>
2022-11-29 13:56:51 +0100

    Add country codes to final list of suffixes

--- merge_suffixes.py
+++ merge_suffixes.py
@@ -6,6 +6,7 @@ input_lists = [
     'data/manual/manual_additions_not_public_suffix.csv',
     'data/processed/parsed_suffixes.csv'
 ]
+tld_country_file = 'data/manual/tld_country.csv'
 
 columns = ['tld', 'suffix', 'category', 'reference']
 df = pd.DataFrame()
@@ -21,5 +22,13 @@ def uniq_join_strip(l):
 # deduplicate suffixes and merge category and reference
 df = df.groupby(['tld', 'suffix'], as_index = False).agg({'category': uniq_join_strip, 'reference': uniq_join_strip})
 df.sort_values(['tld', 'suffix'], inplace=True)
+
+# add country
+tld_country = pd.read_csv(tld_country_file,
+                          index_col='tld',
+                          keep_default_na=False).to_dict()['country']
+df['country'] = df['tld'].apply(lambda tld: tld_country[tld] if tld in tld_country else None)
+output_columns = ['tld', 'suffix', 'country', 'category', 'reference']
+df = df.reindex(columns=output_columns)
 
 df.to_csv(output_file, index=False)
